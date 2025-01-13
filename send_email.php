bed42d05a0f21d9e84a94814be8ba1501808fcf1
Shivank <shivanksengar07@gmail.com>
2025-01-13 19:26:57 +0530

    * Updated Links for test

--- send_email.php
+++ send_email.php
@@ -2,10 +2,10 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    $to = "customercare@lamitek.in";
+    $to = "shivanksengar07@gmail.com";
     $subject = "New Contact Request";
     $message = "I would like to work with you. My email address is: " . $email;
-    $headers = "From: customercare@lamitek.in";
+    $headers = "From: shivanksengar07@gmail.com";
 
     if (mail($to, $subject, $message, $headers)) {
         echo "Email sent successfully!";
